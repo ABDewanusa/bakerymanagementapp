c913ec13bcaa68240b2252876e1e7de723189e58
Bagas Dewanusa <aryasenabagas.work@gmail.com>
2024-04-21 21:27:46 +0700

    US.01.01: empty dashboard

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div className="">
+        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <strong>Welcome to eBakery.</strong>
+        </p>
+        <p className={`${lusitana.className} text-xl text-gray-500 md:text-3xl md:leading-normal`}>
+          It's a management system for bakery's day-to-day operations.
+        </p>
+      </div>
+      <div className="">
+        <Link
+          href="/login"
+          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        >
+          <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+        </Link>
+
+      </div>
+
+      <div className="text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-center">
+        <h4>I want write something <a href="https://medium.com" target="_blank"
+          rel="noopener noreferrer"><strong>here</strong></a>.</h4>
+
+
+
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import { Maiden_Orange } from 'next/font/google';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+        orange: {
+          5: '#FFFDFB',
+          10: '#fffcf8',
+          15: '#fffbf5',
+          50: '#fff3e0',
+          100: '#ffe0b2',
+          200: '#ffcc80',
+          300: '#ffb74d',
+          400: '#ffa726',
+          500: '#ff9800',
+          600: '#fb8c00',
+          700: '#f57c00',
+          800: '#ef6c00',
+          900: '#e65100',
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+export default config;
